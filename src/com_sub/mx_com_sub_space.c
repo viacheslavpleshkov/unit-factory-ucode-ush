e76c5123b4d46e046b5e063c198d55b1ddf0bce4
Viacheslav Pleshkov <work@viacheslavpleshkov.com>
2020-06-16 13:01:45 +0300

    Fix command substitution (segmentation faul)

--- src/com_sub/mx_com_sub_space.c
+++ src/com_sub/mx_com_sub_space.c
@@ -9,6 +9,7 @@ void static find_com_sub(t_com_sub *com_sub, int i) {
         com_sub->space_end_index = i;
     }
 }
+
 static void insert_com_sub(t_com_sub *c, char **data, t_ush *ush) {
     c->temp_str = mx_util_strindup(*data, c->space_first_index + 1,
                                    c->space_end_index);
@@ -18,10 +19,21 @@ static void insert_com_sub(t_com_sub *c, char **data, t_ush *ush) {
     c->temp_str = mx_strjoin(c->temp_join, "`");
 }
 
+static bool is_space_fail(t_com_sub *c, int i)  {
+    if (c->space_first_index == i -1 && c->space_end_index == i) {
+        c->space_first_index = 0;
+        c->space_end_index = 0;
+        return true;
+    }
+    return false;
+}
+
 int mx_com_sub_space(char **data, t_com_sub *c, t_ush *ush, int i) {
     if ((*data)[i] == '`') {
         find_com_sub(c, i);
         if (c->space_first_index < c->space_end_index) {
+            if(is_space_fail(c, i) == true)
+                return 0;
             insert_com_sub(c, data, ush);
             if (c->status == 0 && strlen(c->cout_execute) > 0)
                 c->temp_data = mx_replace_substr(*data, c->temp_str, c->cout_execute);
